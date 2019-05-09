c755e501fd25cc3db617516712fe3c98bd473d0a
alfonsosn <alfonso.en.ca@gmail.com>
2019-05-09 13:43:12 +0100

    created feed screen

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,4 +1,5 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import Feed from '../Containers/Feed'
 import LandingScreen from '../Containers/LandingScreen'
 import LoginScreen from '../Containers/LoginScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
@@ -7,6 +8,7 @@ import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
+  Feed: { screen: Feed },
   LandingScreen: { screen: LandingScreen },
   LoginScreen: { screen: LoginScreen },
   LaunchScreen: { screen: LaunchScreen }
